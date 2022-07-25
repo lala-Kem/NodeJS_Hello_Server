cb86fee4838ede7f01dd0dd0ce352e5b951b7a53
huynd.t1 <huynd.t1@gmail.com>
2022-07-25 23:45:22 +0700

    get users list

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const port = 3000;
 app.get("/", function (req, res) {
     res.send("Hello world!");
 });
+const users = [
+    {
+        name: "Joker",
+        age: 55,
+    },
+    {
+        name: "Batman",
+        age: 35,
+    },
+    {
+        name: "Iron man",
+        age: 25,
+    },
+];
+app.get("/users", function (req, res) {
+    res.send(users);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening to port: ${port}`);
